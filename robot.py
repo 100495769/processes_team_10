fa44d282b5596e8a0920b065b8b6eb893a241229
sergio <100495769@alumnos.uc3m.es>
2024-10-04 18:07:35 +0200

    Added functionality for reading arguments and sigalarm.

--- robot.py
+++ robot.py
@@ -1,4 +1,5 @@
-import sensor, signal, sys
+import os, sys, signal, argparse
+import sensor
 class Robot:
     def __init__(self, identifier: int, position: list, battery: int, room):
         self.position = position
@@ -6,10 +7,10 @@ class Robot:
         self.identifier = identifier
         self.sensor = sensor.Sensor(room)
 
-    def pos(self):
+    def get_position(self):
         return self.position
 
-    def bat(self):
+    def get_battery(self):
         return self.battery
 
     def change_battery(self, change):
@@ -44,18 +45,44 @@ def sig_handler(signo, frame):
     elif(signo==signal.SIGUSR1):
         pass
     elif(signo==signal.SIGALRM):
-        global robot.change_battery(-1)
+        robot.change_battery(-1)
+        signal.alarm(1)
 
-robot = 0
+
+
 def main():
+    #Arguments using argparser
+    parser = argparse.ArgumentParser(description='RCP: Robot Control Program')
+    #Add the possible arguments for our program
+    parser.add_argument('identifier', type=int, help='Robot identifier')
+    parser.add_argument('f', type=str, help='File name of the room.txt')
+    parser.add_argument('-pos', '--position', metavar=('row', 'column'), type=int, nargs=2, default= [0,0], help='Position tuple where robots start. Default: [0, 0]')
+    parser.add_argument('-b', '--battery', type=int, default= 100, help= 'Initial battery level of the robot. Default: 100')
 
+    #Create an instance of the class parse_args() in args, which holds the given arguments
+    args = parser.parse_args()
+    #Uncomment to check how arguments can be accessed from args                     DELETE
+    #print(args.identifier, args.f, args.position, args.battery)
+    #Print PID in stderr
+    print("PID:",os.getpid(), file= sys.stderr)
+
+    #Create an instance of the robot with the attributes given
+    global robot
+    robot = Robot(args.identifier, args.position, args.battery, args.f)
+
+    #Change the signals functionality
     signal.signal(signal.SIGINT, sig_handler)
     signal.signal(signal.SIGQUIT, sig_handler)
     signal.signal(signal.SIGTSTP, sig_handler)
     signal.signal(signal.SIGUSR1, sig_handler)
+    signal.signal(signal.SIGALRM, sig_handler)
 
 
-
+    #Example of the alarm working.
+    signal.alarm(1)
+    while True:
+        signal.pause()
+        print(robot.get_battery())
 
 if __name__ == "__main__":
     main()
